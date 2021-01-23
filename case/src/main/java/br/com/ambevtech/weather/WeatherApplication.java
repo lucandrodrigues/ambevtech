8e573fbe6d2c53af4ba2ab263824dc5d28131b89
Lucas <lucandrodrigues@gmail.com>
2021-01-23 13:01:58 -0300

    commit inicial

---
+++ case/src/main/java/br/com/ambevtech/weather/WeatherApplication.java
@@ -0,0 +1,15 @@
+package br.com.ambevtech.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+}
---
+++ case/src/main/java/br/com/ambevtech/weather/config/CacheNames.java
@@ -0,0 +1,10 @@
+package br.com.ambevtech.weather.config;
+
+public class CacheNames {
+
+    public static Long cacheDefaultTime = 15L;
+
+    public static String cachePrevisao = "cachePrevisao";
+
+    public static String cacheListarCidade = "cacheListarCidade";
+}
